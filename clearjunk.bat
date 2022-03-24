6a445caf7e3404c970dc32a519d8461b58c80289
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-03-24 21:15:24 +0530

    4windows
    Cleans Junk Files

---
+++ clearjunk.bat
@@ -0,0 +1,11 @@
+del /s /f /q %USERPROFILE%\Recent\*
+
+del /s /f /q C:\Windows\Prefetch\*
+
+del /s /f /q C:\Windows\Temp\*
+
+del /s /f /q %USERPROFILE%\appdata\local\temp\*
+
+ipconfig /flushdns
+
+pause
